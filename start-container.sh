3784c33bf4bdfacffd6c98dc6e80ed34fdfa50c6
Michael Chi <chicong@outlook.com>
2018-02-06 18:32:59 +0800

    Edit start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -2,7 +2,6 @@
 
 # Start master
 master_name="hadoop-master"
-docker stop ${master_name} > /dev/null 2>&1
 echo "start ${master_name}"
 docker start ${master_name} > /dev/null
 
@@ -11,7 +10,6 @@ n=3
 i=1
 while [ "$i" -le "$n" ]; do
     tmp_slave_name="hadoop-slave-$i"
-    docker stop ${tmp_slave_name} > /dev/null 2>&1
     echo "start ${tmp_slave_name}"
     docker start ${tmp_slave_name} > /dev/null
     i=$(($i + 1))
