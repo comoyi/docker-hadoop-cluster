9a4ca9b89323984da01203867f0490e8931d2773
Michael Chi <chicong@outlook.com>
2018-02-06 12:03:33 +0800

    Can run word-count example now

--- word-count.sh
+++ word-count.sh
@@ -7,4 +7,7 @@ echo "Hello Hadoop" > word-count/file2.txt
 
 hadoop fs -mkdir -p input
 hadoop fs -put word-count input
-# hadoop jar /opt/hadoop-3.0.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.0.0.jar wordcount input output
+
+hadoop jar /opt/hadoop-3.0.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.0.0.jar wordcount input/word-count output
+
+hadoop fs -cat output/part-r-00000
