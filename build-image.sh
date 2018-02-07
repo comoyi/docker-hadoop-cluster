d68c54c01fe5168e554a4a8fc6687cc835d97bb5
Michael Chi <chicong@outlook.com>
2018-02-07 18:50:44 +0800

    Colorful

--- build-image.sh
+++ build-image.sh
@@ -5,15 +5,14 @@ image_version=0.0.1
 
 # Download
 if [[ ! -f "hadoop-${hadoop_version}.tar.gz" ]]; then
-    echo "hadoop-${hadoop_version}.tar.gz not exist start download..."
-    echo ""
+    printf "\033[33mhadoop-${hadoop_version}.tar.gz not exist start download...\033[0m\n"
     wget http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-${hadoop_version}/hadoop-${hadoop_version}.tar.gz
 fi
 
 # Check
 sha256sum -c hadoop.sha256sum > /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-    echo "Check sha256sum error, please delete hadoop-${hadoop_version}.tar.gz and rerun this script."
+    printf "\033[31mCheck sha256sum error, please delete hadoop-${hadoop_version}.tar.gz and rerun this script.\033[0m\n"
     exit 1
 fi
 
--- start-container.sh
+++ start-container.sh
@@ -20,5 +20,4 @@ echo ""
 docker ps | head -1
 docker ps | grep hadoop
 
-echo ""
-echo "you can access ${master_name} by command: docker exec -it ${master_name} bash"
+printf "\n\033[0m[OK]\033[0m You can access ${master_name} by command: ${color_green}docker exec -it ${master_name} bash\033[0m\n"
